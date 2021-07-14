c90ea2cafe7971d9350429b6c309dc204403198e
CatClawed <loser@barrelwisdom.com>
2021-07-14 19:08:22 +0000

    I think just the migrations are different? I'll figure it out, this is the version of the DB I'm using either way.

--- Database/Database/settings.py
+++ Database/Database/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+#ALLOWED_HOSTS = ['barrelwisdom.com', 'test.barrelwisdom.com', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = ['barrelwisdom.com', 'test.barrelwisdom.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
