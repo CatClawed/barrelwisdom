5c2c02ce46e8bf2b12bfbc8cfe276cfc3208ee7e
CatClawed <chloe_the_dork@outlook.com>
2021-11-25 05:25:34 +0000

    Gotta do my settings right.

--- backend/Database/settings.py
+++ backend/Database/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-#ALLOWED_HOSTS = ['barrelwisdom.com', 'test.barrelwisdom.com', 'localhost', '127.0.0.1']
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+if not DEBUG:
+    ALLOWED_HOSTS = ['barrelwisdom.com', 'test.barrelwisdom.com', 'localhost', '127.0.0.1']
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
