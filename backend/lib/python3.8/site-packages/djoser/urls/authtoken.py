b58fafc2ada47c62f26b1c5019c6147ec2c593c7
CatClawed <chloe_the_dork@outlook.com>
2021-11-25 04:59:12 +0000

    BRSL fragments, a couple tweaks. Part 2.

--- backend/lib/python3.8/site-packages/djoser/urls/authtoken.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from djoser import views
-
-urlpatterns = [
-    re_path(r"^token/login/?$", views.TokenCreateView.as_view(), name="login"),
-    re_path(r"^token/logout/?$", views.TokenDestroyView.as_view(), name="logout"),
-]
