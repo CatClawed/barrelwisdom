eb9f990299e1b6d5bb6c70b64ba83021d6d0ebdf
CatClawed <curry@barrelwisdom.com>
2026-07-22 02:37:25 -0600

    Blog fine enough to move to the real stuff.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import path from 'path';
 
 // https://astro.build/config
